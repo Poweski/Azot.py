f77d69f497f2b5ac91b74fa7d878f85312c9580c
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2024-06-05 20:20:32 +0200

    deleted email app keys

--- azot-backend/azot/settings.py
+++ azot-backend/azot/settings.py
@@ -122,8 +122,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gergent.51@gmail.com'
-EMAIL_HOST_PASSWORD = 'mahfxmatpekqtkxw'
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD = ''
 
 
 APPLICATION_PORT = 8080
